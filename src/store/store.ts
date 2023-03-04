a977ba590353d3e73c53adbaf07f7bbfee8476b8
Temka Artemka <yandex949@gmail.com>
2023-03-04 18:39:38 +0300

    build: NODE_ENV show testing

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,10 +2,13 @@ import { configureStore } from "@reduxjs/toolkit"
 import gameReducer from "./features/game/gameSlice"
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
 
+const { REACT_APP_NODE_ENV } = process.env
+
 let opt = { devTools: true }
 if (process.env.NODE_ENV === "production") {
 	opt.devTools = false
 }
+console.log({ ENVIRO1231123: process.env.NODE_ENV })
 
 export const store = configureStore({
 	reducer: {
